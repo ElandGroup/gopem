85f351e19b557236f645f8119f700f6b022fa78e
xiao.xinmiao <xiaoxm_001@13..com>
2016-12-31 13:19:14 +0800

    optimize cert method

--- main.go
+++ main.go
@@ -4,11 +4,10 @@ import (
 	"crypto/tls"
 	"crypto/x509"
 	"flag"
-	"goRequest/logHelper"
 	"io/ioutil"
-	"log"
 	"net/http"
 
+	"epay.api.go/common/smt"
 	"github.com/smallnest/goreq"
 )
 
@@ -19,24 +18,25 @@ func main() {
 		keyFile  = flag.String("key", "./cert/apiclient_key.pem", "A PEM encoded private key file.")
 		caFile   = flag.String("CA", "./cert/rootca.pem", "A PEM eoncoded CA's certificate file.")
 	)
-
-	ReqXmlCert(CertTransport(certFile, keyFile, caFile))
+	if t, err := CertTransport(certFile, keyFile, caFile); err == nil {
+		ReqXmlCert(t)
+	}
 
 	//ReqXml()
 }
 
-func CertTransport(certFile *string, keyFile *string, caFile *string) *http.Transport {
+func CertTransport(certFile *string, keyFile *string, caFile *string) (transport *http.Transport, err error) {
 	flag.Parse()
 	// Load client cert
 	cert, err := tls.LoadX509KeyPair(*certFile, *keyFile)
 	if err != nil {
-		log.Fatal(err)
+		return nil, err
 	}
 
 	// Load CA cert
 	caCert, err := ioutil.ReadFile(*caFile)
 	if err != nil {
-		log.Fatal(err)
+		return nil, err
 	}
 	caCertPool := x509.NewCertPool()
 	caCertPool.AppendCertsFromPEM(caCert)
@@ -47,8 +47,7 @@ func CertTransport(certFile *string, keyFile *string, caFile *string) *http.Tran
 		RootCAs:      caCertPool,
 	}
 	tlsConfig.BuildNameToCertificate()
-	transport := &http.Transport{TLSClientConfig: tlsConfig}
-	return transport
+	return &http.Transport{TLSClientConfig: tlsConfig}, nil
 }
 
 //request with cert
@@ -64,10 +63,10 @@ func ReqXmlCert(transport *http.Transport) {
 
 	req, body, err := reqNew.Post("https://api.mch.weixin.qq.com/secapi/pay/refund").ContentType("xml").SendRawString(xmldata).End()
 	if req.StatusCode == http.StatusOK {
-		logHelper.Debug.Println("\n body:", body)
+		smt.Debug.Println("\n body:", body)
 	}
-	logHelper.Debug.Println("\n err:", err)
-	logHelper.Debug.Println("\n req:", req)
+	smt.Debug.Println("\n err:", err)
+	smt.Debug.Println("\n req:", req)
 
 }
 
@@ -79,9 +78,9 @@ func ReqXml() {
 	`
 	req, body, err := goreq.New().Post("https://api.mch.weixin.qq.com/secapi/pay/refund").ContentType("xml").SendRawString(xmldata).End()
 	if req.StatusCode == http.StatusOK {
-		logHelper.Debug.Println("\n body:", body)
+		smt.Debug.Println("\n body:", body)
 	}
-	logHelper.Debug.Println("\n err:", err)
-	logHelper.Debug.Println("\n req:", req)
+	smt.Debug.Println("\n err:", err)
+	smt.Debug.Println("\n req:", req)
 
 }
